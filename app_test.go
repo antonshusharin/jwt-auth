a6cc8da85d070d15ba6d62bc364f46f9b27378a8
Anton Shusharin <antonshusharin01@gmail.com>
2024-12-13 22:53:36 +0300

    Use require instead of assert in most of the tests

--- app_test.go
+++ app_test.go
@@ -69,35 +69,35 @@ func makeTestRequest(req *http.Request) (statusCode int, responseBody []byte) {
 }
 
 func TestGetAndRefreshToken(t *testing.T) {
-	assert := assert.New(t)
+	require := require.New(t)
 	body := strings.NewReader(fmt.Sprintf(`{"guid": "%v"}`, MOCK_USERS["Ivan"].guid))
 	req := httptest.NewRequest("POST", "/get-token", body)
-	assert.NotNil(req)
 	status, respBody := makeTestRequest(req)
-	assert.Equal(http.StatusOK, status, "Could not retrieve access token")
+	require.Equal(http.StatusOK, status, "Could not retrieve access token")
 	var tokenPair TokenPair
 	err := json.Unmarshal(respBody, &tokenPair)
-	assert.NoError(err, "Could not parse token pair as JSON")
+	require.NoError(err, "Could not parse token pair as JSON")
 	body2, _ := json.Marshal(tokenPair)
 	req = httptest.NewRequest("POST", "/refresh-token", bytes.NewReader(body2))
 	status, respBody = makeTestRequest(req)
-	assert.Equal(http.StatusOK, status, "Could not refresh with provided token pair")
+	require.Equal(http.StatusOK, status, "Could not refresh with provided token pair")
 	err = json.Unmarshal(respBody, &tokenPair)
-	assert.NoError(err, "Could not decode refreshed token pair")
+	require.NoError(err, "Could not decode refreshed token pair")
 	req = httptest.NewRequest("POST", "/refresh-token", bytes.NewReader(body2))
 	status, _ = makeTestRequest(req)
-	assert.Equal(http.StatusForbidden, status, "Was able to refresh the same token pair twice")
+	require.Equal(http.StatusForbidden, status, "Was able to refresh the same token pair twice")
 }
 
 func TestJWTSecurity(t *testing.T) {
 	assert := assert.New(t)
+	require := require.New(t)
 	conn, err := app.db.Acquire(context.Background())
-	require.NoError(t, err, "Could not connect to database")
+	require.NoError(err, "Could not connect to database")
 	defer conn.Release()
 	pair1, err := app.generateTokenPair(conn, MOCK_USERS["Ivan"].guid, "192.0.2.1")
-	require.NoError(t, err, "Could not create token pair 1")
+	require.NoError(err, "Could not create token pair 1")
 	pair2, err := app.generateTokenPair(conn, MOCK_USERS["Maria"].guid, "192.0.2.1")
-	require.NoError(t, err, "Could not create token pair 2")
+	require.NoError(err, "Could not create token pair 2")
 	badPair := TokenPair{pair1.Access, pair2.Refresh}
 	body, _ := json.Marshal(badPair)
 	req := httptest.NewRequest("POST", "/refresh-token", bytes.NewReader(body))
@@ -108,9 +108,9 @@ func TestJWTSecurity(t *testing.T) {
 	req = httptest.NewRequest("POST", "/refresh-token", bytes.NewReader(body))
 	req.RemoteAddr = "192.0.2.2"
 	status, _ = makeTestRequest(req)
-	assert.Equal(http.StatusOK, status, "Could not refresh pair 2, which is valid")
+	require.Equal(http.StatusOK, status, "Could not refresh pair 2, which is valid")
 	mailer := app.mailer.(*TestingMailer)
 	mail := mailer.CheckEmail()
 	require.NotNil(t, mailer, "Warning email was not sent")
-	assert.Equal([]string{MOCK_USERS["Maria"].email}, mail.To, "Warning email was sent to the wrong user")
+	require.Equal([]string{MOCK_USERS["Maria"].email}, mail.To, "Warning email was sent to the wrong user")
 }
--- tokens_test.go
+++ tokens_test.go
@@ -5,33 +5,33 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRefreshTokenRoundtrip(t *testing.T) {
-	assert := assert.New(t)
+	require := require.New(t)
 	token := RefreshToken{RefreshUUID: uuid.New(), ClientIp: "127.0.0.1"}
 	encoded, err := json.Marshal(&token)
-	assert.NoError(err, "Could not encode token")
+	require.NoError(err, "Could not encode token")
 	var token2 RefreshToken
 	err = json.Unmarshal(encoded, &token2)
-	assert.NoError(err, "Could not decode token")
-	assert.Equal(token, token2, "Encoded and decoded tokens do not match")
+	require.NoError(err, "Could not decode token")
+	require.Equal(token, token2, "Encoded and decoded tokens do not match")
 }
 
 func TestRefreshTokensecure(t *testing.T) {
-	assert := assert.New(t)
+	require := require.New(t)
 	uid := uuid.New()
 	uid2 := uuid.New()
 	token := RefreshToken{RefreshUUID: uid, ClientIp: "127.0.0.1"}
 	tokenHash, err := token.HashBcrypt()
-	assert.NoError(err, "Unable to hash token")
+	require.NoError(err, "Unable to hash token")
 	err = token.ValidateHash(tokenHash)
-	assert.NoError(err, "Token did not validate against its own hash")
+	require.NoError(err, "Token did not validate against its own hash")
 	badToken := token
 	badToken.RefreshUUID = uid2
 	badTokenHash, err := badToken.HashBcrypt()
-	assert.NoError(err, "Unable to hash token")
+	require.NoError(err, "Unable to hash token")
 	err = token.ValidateHash(badTokenHash)
-	assert.Error(err, "Validated a bad token hash")
+	require.Error(err, "Validated a bad token hash")
 }
